f1ea4bab53832ba97be1e47f3fa669f12eb5ad83
Arthur Monteath Trindade <arthur.monteath@gmail.com>
2025-11-02 10:25:46 -0500

    added end game screen

--- assets/ui/credits.gd
+++ assets/ui/credits.gd
@@ -4,4 +4,8 @@ extends Control
 
 func _ready() -> void:
 	credit_animation_player.play("credit_animation")
+	leave_credits()
 	
+func leave_credits():
+	await get_tree().create_timer(9.0).timeout
+	get_tree().change_scene_to_file("res://assets/ui/menu.tscn")
--- game_end.gd
+++ game_end.gd
@@ -1,11 +1,16 @@
 extends Control
 
-@onready var player_label: Label = $VBoxContainer/Player
-@onready var score_1: Label = $VBoxContainer/Score1
-@onready var score_2: Label = $VBoxContainer/Score2
+@onready var player_label: Label = %PlayerLabel
+@onready var score_1: Label = %Score1
+@onready var score_2: Label = %Score2
 
 func _ready() -> void:
-	player_label.text = PlayerScore.get_winner() + " won!"
+	player_label.text = PlayerScore.get_winner() + " wins!"
 	
 	score_1.text = "Player1 scored " + str(int(PlayerScore.score1))
 	score_2.text = "Player2 scored " + str(int(PlayerScore.score2))
+
+
+func _on_button_pressed() -> void:
+	PlayerScore.restart()
+	get_tree().change_scene_to_file("res://assets/ui/menu.tscn")
